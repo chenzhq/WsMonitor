15715b01bc57fb4a856784472fda29d7a01dd2ef
zhongstar <569528872@qq.com>
2017-06-16 09:54:37 +0800

    feat: 完成组合三种类型的业务数量RestController

--- src/main/java/com/ws/stoner/controller/rest/DashboardRestController.java
+++ src/main/java/com/ws/stoner/controller/rest/DashboardRestController.java
@@ -1,17 +1,14 @@
 package com.ws.stoner.controller.rest;
 
 import com.ws.stoner.constant.ResponseErrorEnum;
-import com.ws.stoner.exception.ManagerException;
-import com.ws.stoner.manager.HostManager;
-import com.ws.stoner.manager.PlatformManager;
+import com.ws.stoner.exception.ServiceException;
 import com.ws.stoner.model.dto.StateNumDTO;
+import com.ws.stoner.service.CountStateService;
 import com.ws.stoner.utils.RestResultGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 import static com.ws.stoner.constant.MessageConsts.REST_RESPONSE_SUCCESS;
 
@@ -21,28 +18,36 @@ import static com.ws.stoner.constant.MessageConsts.REST_RESPONSE_SUCCESS;
 @RestController
 public class DashboardRestController {
     @Autowired
-    private HostManager hostManager;
-
-    @Autowired
-    private PlatformManager platformManager;
+    private CountStateService countStateService;
 
     @RequestMapping(value = "host/count", method = RequestMethod.GET)
     public String countHost() {
         StateNumDTO allHostNum;
         try {
-            allHostNum = hostManager.countAllHostState();
-        } catch (ManagerException e) {
+            allHostNum = countStateService.countHostState();
+        } catch (ServiceException e) {
             return RestResultGenerator.genErrorResult(ResponseErrorEnum.SERVICE_HANDLE_ERROR).toString();
         }
         return RestResultGenerator.genResult(allHostNum, REST_RESPONSE_SUCCESS).toString();
     }
 
+    @RequestMapping(value = "point/count",method = RequestMethod.GET)
+    public String countPoint() {
+        StateNumDTO allPointNum;
+        try {
+            allPointNum = countStateService.countPointState();
+        } catch (ServiceException e) {
+            return RestResultGenerator.genErrorResult(ResponseErrorEnum.SERVICE_HANDLE_ERROR).toString();
+        }
+        return RestResultGenerator.genResult(allPointNum, REST_RESPONSE_SUCCESS).toString();
+    }
+
     @RequestMapping(value = "platform/count", method = RequestMethod.GET)
     public String countPlatform() {
-        List<StateNumDTO> allPlatformStatusNum;
+        StateNumDTO allPlatformStatusNum;
         try {
-            allPlatformStatusNum = platformManager.countAllPlatform();
-        } catch (ManagerException e) {
+            allPlatformStatusNum = countStateService.countPlatformState();
+        } catch (ServiceException e) {
             return RestResultGenerator.genErrorResult(ResponseErrorEnum.SERVICE_HANDLE_ERROR).toString();
         }
         return RestResultGenerator.genResult(allPlatformStatusNum, REST_RESPONSE_SUCCESS).toString();
