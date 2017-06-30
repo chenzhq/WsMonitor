3de09342b044ddf6d9c8d3489135b0ad14dc61a2
zhongstar <569528872@qq.com>
2017-06-30 10:36:12 +0800

    feat: 实现设备分组列表获取功能 listMongo()
    全部完成，递归调用树形结构并组装数据,MongoGroupServiceImpl()实现

--- src/test/java/com/ws/stoner/service/impl/MongoGroupServiceImplTest.java
+++ src/test/java/com/ws/stoner/service/impl/MongoGroupServiceImplTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /** 
@@ -114,7 +115,14 @@ public void testFindAll() throws Exception {
     @Test
     public void testListMongoTree() throws Exception{
         List<MongoGroupVO> mongoGroupVOS = mongoGroupService.getGroupTree("root",new ArrayList<MongoGroupVO>());
+        Collections.reverse(mongoGroupVOS);
         System.out.println(mongoGroupVOS);
+    }
+
+    @Test
+    public void testListMongo() throws Exception {
+    List<MongoGroupVO> mongoGroupVOS = mongoGroupService.listMongo();
+    System.out.println(mongoGroupVOS);
     }
 
 
