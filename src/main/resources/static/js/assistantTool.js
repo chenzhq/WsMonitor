34b0297a77daddccd16a8085ece149443326d1d4
yanms719 <447876527@qq.com>
2017-08-15 15:39:52 +0800

    Merge branch 'develop' of E:\maven\WsMonitor\WsMonitor with conflicts.

---
+++ src/main/resources/static/js/assistantTool.js
@@ -0,0 +1,39 @@
+/**
+ * Created by yanms on 2017/8/15.
+ */
+//告警消息
+function errorMsg_no_connect(title) {
+    Lobibox.notify("error", {
+        size: "normal",
+        rounded: false,
+        delayIndicator: true,
+        msg: "连接服务器失败！",
+        icon: "warning icon",
+        title: title
+    });
+}
+function errorMsg_no_data(title) {
+    Lobibox.notify("error", {
+        size: "normal",
+        rounded: false,
+        delayIndicator: true,
+        msg: "解析数据失败！",
+        icon: "warning icon",
+        title: title
+    });
+}
+//判断字符串长度
+function strlen(str){
+    var len = 0;
+    for (var i=0; i<str.length; i++) {
+        var c = str.charCodeAt(i);
+        //单字节加1
+        if ((c >= 0x0001 && c <= 0x007e) || (0xff60<=c && c<=0xff9f)) {
+            len++;
+        }
+        else {
+            len+=2;
+        }
+    }
+    return len;
+}
