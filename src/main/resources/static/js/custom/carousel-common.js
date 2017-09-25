cb16b40d94cf510814e63f5e2864fc5502d73e99
zhongstar <569528872@qq.com>
2017-09-25 18:09:59 +0800

    fix: 修复轮播通用js

--- src/main/resources/static/js/custom/carousel-common.js
+++ src/main/resources/static/js/custom/carousel-common.js
@@ -4,7 +4,7 @@
  */
 
 //渲染 下拉框 通用方法  data 为数组字符串的方法
-function getSelectByString($_target, data) {
+function getSelectByString($_target,$_default, data) {
     if (!$_target instanceof jQuery) {
         console.error("参数对象必须是jquery对象")
     }
@@ -15,12 +15,12 @@ function getSelectByString($_target, data) {
     }
     $_target.html(str);
     //默认选择第一个
-    $_target.dropdown('set text',data[0]);
-    $_target.dropdown('set value',data[0]);
+    $_default.dropdown('set text',data[data.length-1]);
+    $_default.dropdown('set value',data[data.length-1]);
 }
 
 //渲染 下拉框 通用方法  data 为数组对象的方法
-function getSelectByObject($_target, data) {
+function getSelectByObject($_target,$_default, data) {
     if (!$_target instanceof jQuery) {
         console.error("参数对象必须是jquery对象")
     }
@@ -31,201 +31,13 @@ function getSelectByObject($_target, data) {
     }
     $_target.html(str);
     //默认选择第一个
-    $_target.dropdown('set text',data[0].name);
-    $_target.dropdown('set value',data[0].type);
-}
-
-function getSelectPoint(data) {
-    var str = '';
-    for(var i=0;i<data.length;i++)
-    {
-        str +="<div class='item' data-value='"+data[i].point_id+"'  >"+data[i].point_name+"</div> ";
-    }
-    $_target.html(str);
-    //默认选择第一个
-    $_target.dropdown('set text',data[0].point_name);
-    $_target.dropdown('set value',data[0].point_id);
-}
-
-//展示页数据 渲染 page_vo 全局变量，在调用之前已经通过 API 获取到了 page_vo 的值
-function darwPage() {
-    var layout_arr = page_vo.layout_data;
-    var config_arr = page_vo.config_data;
-    var block_arr = page_vo.block_data;
-    for(x in layout_arr) {
-        //第一步： 确定布局
-        //console.log('x',x);
-        //dom 选择需要根据具体情况而定
-        $('').gridster({
-            widget_margins: [x.col, x.row ],
-            widget_base_dimensions: [x.x_size, x.y_size]
-        });
-        //第二步： 根据配置画图
-        if(config_arr[x].block_type === 'view') {
-            //视图
-            if(config_arr[x].graph_type === 'statepie') {
-                //状态视图
-                var host_chart = echarts.init(document.getElementById(''));//待补充
-                var host_option={
-                    title: {
-                        text: '0',
-                        subtext: '台设备',
-                        x: 'center',
-                        y: 'center',
-                        itemGap: 1,
-                        textStyle: {
-                            color: '#01AAED',
-                            fontFamily: '微软雅黑',
-                            fontSize: 24,
-                            fontWeight: 'bolder'
-                        }
-                    },
-                    tooltip: {
-                        trigger: 'item',
-                        formatter: "{a} <br/>{b} : {c} ({d}%)"
-                    },
-                    legend: {
-                        orient: 'vertical',
-                        x: 'right',
-                        y: 'center',
-                        data:['正常','警告','严重'],
-                    },
-                    grid: {
-                        left:'5%',
-                        right:'5%',
-                    },
-                    series: [
-                        {
-                            name: '访问来源',
-                            type: 'pie',
-                            radius: ['40%', '65%'],
-                            selectedMode: '',
-                            itemStyle: {
-                                normal: {
-                                    label: {
-                                        show: true,
-                                        formatter: function (param) {
-                                            return param.name + param.value + "台" + ':\n' + Math.round(param.percent) + '%';
-                                        },
-                                        textStyle: {
-                                            fontWeight: 'bolder'
-                                        }
-                                    }
-                                },
-                            },
-                            data:  [
-                                {
-                                    "value": 2,
-                                    "name": "正常,"
-                                },
-                                {
-                                    "value": 7,
-                                    "name": "警告"
-                                }
-                            ],
-                            color: Color
-                        }
-                    ]
-                };
-                host_chart.setOption(host_option);
-                var point_chart = echarts.init(document.getElementById(''));//待补充
-                var point_option = {
-                    title: {
-                        text: '0',
-                        subtext: '个监控点',
-                        x: 'center',
-                        y: 'center',
-                        itemGap: 1,
-                        textStyle: {
-                            color: '#01AAED',
-                            fontFamily: '微软雅黑',
-                            fontSize: 24,
-                            fontWeight: 'bolder'
-                        }
-                    },
-                    tooltip: {
-                        trigger: 'item',
-                        formatter: "{a} <br/>{b} : {c} ({d}%)"
-                    },
-                    legend: {
-                        orient: 'vertical',
-                        x: 'right',
-                        y: 'center',
-                        data:['正常','警告','严重'],
-                    },
-                    grid: {
-                        left:'15%',
-                        right:'15%',
-                    },
-                    series: [
-                        {
-                            name: '访问来源',
-                            type: 'pie',
-                            radius: ['40%', '65%'],
-                            selectedMode: '',
-                            itemStyle: {
-                                normal: {
-                                    label: {
-                                        show: true,
-                                        formatter: function (param) {
-                                            return param.name + param.value + "个" + ':\n' + Math.round(param.percent) + '%';
-                                        },
-                                        textStyle: {
-                                            fontWeight: 'bolder'
-                                        }
-                                    }
-                                },
-                            },
-                            data:  [
-                                {
-                                    "value": 5,
-                                    "name": "正常"
-                                },
-                                {
-                                    "value": 5,
-                                    "name": "警告"
-                                }
-                            ],
-                            color: Color
-                        }
-                    ]
-                }
-                point_chart.setOption(point_option);
-                getViewStatepie(host_chart,point_chart,block_arr[x]);
-
-            }else if(config_arr[x].graph_type === 'problems') {
-                //问题视图
-                //待完成
-            }else if(config_arr[x].graph_type === 'appletree') {
-                //苹果树
-            }else {
-
-            }
-        }else if(config_arr[x].block_type === 'graph') {
-            //图表
-            graph_vo = block_arr[x];
-            //渲染图形
-            var graph_chart = echarts.init(document.getElementById(''));//待补充
-            var option = getGraphOption(config_arr[x].graph_type,graph_vo);
-            graph_chart.setOption(option);
-        }else if(config_arr[x].block_type === 'chart') {
-            //控件
-            if(config_arr[x].graph_type === 'clock') {
-                //钟表
-                $('').html(block_arr[x].time);//待完成
-            }else if (config_arr[x].graph_type === 'table') {
-                //表格
-                $('').html(block_arr[x].html_contents);// 待完成
-            }
-        }else {
-
-        }
-
-    }
-
+    $_default.dropdown('set text',data[data.length-1].name);
+    $_default.dropdown('set value',data[data.length-1].type);
 }
 //后期整理还可以继续优化，将图表单独拿出来先初始化，后面根据赋值修改图形数据
 function getGraphOption(graph_type,graph_vo) {
+//        console.log("type",graph_type);
+//        console.log("graph_vo",graph_vo);
     var option ;
     var ymax ;
     if(graph_vo.units === '%'){
@@ -351,7 +163,7 @@ function getGraphOption(graph_type,graph_vo) {
                 },
                 series: [{
                     name: graph_vo.block_name,
-                    type: graph_vo.graph_type,
+                    type: graph_type,
                     data: graph_vo.data,
                     color: [],
                 }]
@@ -360,19 +172,3 @@ function getGraphOption(graph_type,graph_vo) {
     return option;
 }
 
-function getViewStatepie(host_chart,point_chart,data) {
-    console.log("state-data",data);
-    var hostStateNum = data.hostStateNum;
-    if (hostStateNum.stateNum) {
-        host_option.title.text = hostStateNum.totalNum;
-        host_option.series[0].data = hostStateNum.stateNum;
-        // 重写指定图表的配置项和数据
-        host_chart.setOption(host_option);
-    }
-    var pointStateNum = data.pointStateNum;
-    if (pointStateNum.stateNum) {
-        point_option.title.text = pointStateNum.totalNum;
-        point_option.series[0].data = pointStateNum.stateNum;
-        point_chart.setOption(point_option);
-    }
-}
