2e3572c59e7dee7a6406c9df86077caead02ee73
yanms719 <447876527@qq.com>
2017-09-29 15:18:59 +0800

    feat: 可编辑列表插件及实现

---
+++ src/main/resources/static/js/custom-editable.js
@@ -0,0 +1,26 @@
+﻿//editable page table settings
+'use strict'
+function custom_edittable(fields,str) {
+    $("#jsGrid").jsGrid({
+        /*width: "100%",*/
+        width: str+68,
+        height: "200%",
+
+        filtering: false,
+        editing: true,
+        sorting: true,
+        paging: true,
+
+        data: db.clients,
+        fields:fields
+        /*fields: [
+            {name: "Name", type: "text", width: 250},
+            {name: "Age", type: "text", width: 250},
+            {name: "Address", type: "text", width: 300},
+            {name: "Country", type: "text", width: 200},
+            {type: "control"}
+        ]*/
+    });
+}
+$('#data_table').editableTableWidget();
+$('#data_table').DataTable();
