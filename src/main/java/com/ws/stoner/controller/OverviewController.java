438d4146f7def393d4ee28e2c6d701179b4e8ca9
zhongstar <569528872@qq.com>
2017-10-17 16:29:10 +0800

    feat: 设备概览状态数量统计API

--- src/main/java/com/ws/stoner/controller/OverviewController.java
+++ src/main/java/com/ws/stoner/controller/OverviewController.java
@@ -1,7 +1,17 @@
 package com.ws.stoner.controller;
 
+import com.ws.stoner.exception.ServiceException;
+import com.ws.stoner.model.dto.BriefHostDTO;
+import com.ws.stoner.model.view.statepie.StateViewVO;
+import com.ws.stoner.service.HostService;
+import com.ws.stoner.service.ViewService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by chenzheqi on 2017/7/11.
@@ -10,8 +20,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/overview")
 public class OverviewController {
 
+    @Autowired
+    private ViewService viewService;
+
+    @Autowired
+    private HostService hostService;
+
     @RequestMapping(value = {"/", ""})
-    public String overview() {
-        return "overview";
+    public ModelAndView overview() throws ServiceException{
+        ModelAndView mav = new ModelAndView("overview");
+        List<BriefHostDTO> hostDTOS = hostService.listAllHost();
+        List<String> hostIds = new ArrayList<>();
+        for(BriefHostDTO hostDTO : hostDTOS) {
+            hostIds.add(hostDTO.getHostId());
+        }
+        StateViewVO stateViewVO = viewService.getStateViewByHostIds(hostIds);
+        mav.addObject("stateViewVO", stateViewVO );
+        return mav;
     }
 }
