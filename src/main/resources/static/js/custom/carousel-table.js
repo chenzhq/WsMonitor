44d0d5ba492d2de9062d416c48367954ed6a63ec
chenzhq <chenzheqi.czq@163.com>
2017-10-18 15:56:47 +0800

    feat: 增加静态表格编辑功能

--- src/main/resources/static/js/custom/carousel-table.js
+++ src/main/resources/static/js/custom/carousel-table.js
@@ -3,23 +3,35 @@
  */
 /**
  * 创建静态表格
- * @param options   rows cols 行数和列数
- *                  modalId 弹出框modal的ID
+ * @param options   rows cols   行数和列数
+ *                  modalId     弹出框modal的ID
+ *                  editMode    是否为编辑状态
+ *                  values      如果是编辑状态，默认文本数组，从左至右，由上到下
  */
 function genTableBlock(options) {
     const settings = $.extend({
         rows: 1,
         cols: 1,
         modalId: '',
+        editMode: false,
+        values: ['1', '2', '3', '4']
     }, options);
 
     const modal = document.getElementById(settings.modalId);
 
-    if (!modal.innerHTML) {
-        modal.innerHTML = genModal(settings.rows, settings.cols);
+    if (settings.editMode) {
+        if (settings.values.length !== settings.rows * settings.cols) {
+            console.error('单元格数量与数据长度不对应');
+            return;
+        }
+    }
+
+    console.log(settings)
+    if (!modal.innerHTML || modal.innerHTML.length === 0) {
+        modal.innerHTML = genModal(settings.rows, settings.cols, settings.values, settings.editMode);
     } else {
         let editedTable = document.getElementById('edited-table');
-        editedTable.innerHTML = genTableHtml(settings.rows, settings.cols);
+        editedTable.innerHTML = genTableHtml(settings.rows, settings.cols, settings.values, settings.editMode);
     }
 
     let $modal = $(modal);
@@ -80,8 +92,8 @@ function genTableBlock(options) {
 }
 
 
-function genModal(row, col) {
-    let tableHtml = '<div id="edited-table">' + genTableHtml(row, col) + '</div>';
+function genModal(row, col, values, edited) {
+    let tableHtml = '<div id="edited-table">' + genTableHtml(row, col, values, edited) + '</div>';
     let header_section = '<div class="header">' +
         '填写表格' +
         '</div>';
@@ -96,14 +108,23 @@ function genModal(row, col) {
     return header_section + tableHtml + action_section;
 }
 
-function genTableHtml(row, col, values) {
+function genTableHtml(row, col, values, edited) {
     let body = '';
     for (let i = 0; i < row; ++i) {
         body += '<tr>';
-        if (values) {
+        if (values && !edited) {
             for (let j = 0; j < col; ++j) {
                 body += '<td>' +
                     values[i * col + j] +
+                    '</td>';
+            }
+        } else if (values && edited) {
+            console.log('edit')
+            for (let j = 0; j < col; ++j) {
+                body += '<td>' +
+                    '<div id="cell-' + i + '-' + j + '" class="ui large fluid transparent input" style="margin-bottom: 0">' +
+                    '  <input placeholder="..." ' + 'value="' + values[i * col + j] + '">' +
+                    '</div>' +
                     '</td>';
             }
         } else {
